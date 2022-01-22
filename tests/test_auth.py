b2cac197ec613e211811b43ae1a718cdad274437
Elison Crum <elisoncrum@gmail.com>
2022-01-22 10:45:52 -0800

    Testing

---
+++ tests/test_auth.py
@@ -0,0 +1,20 @@
+from flask import request
+from flask import current_app
+from fixtures import client
+
+def test_index(client):
+    response = client.get('/auth/login', follow_redirects = True)
+    
+    assert request.base_url == current_app.config["UAA_AUTHORIZE_URI"]
+
+    print(request.base_url)
+    response = client.post(
+        request.base_url,
+        data = {
+            "username": "paul",
+            "password": "wombat",
+        },
+        follow_redirects = True
+    )
+
+    print(response)
--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,14 +1,4 @@
-import os
-import tempfile
-import pytest
-from main import create_app
-
-@pytest.fixture
-def client():
-    app = create_app()
-
-    with app.test_client() as client:
-        yield client
+from fixtures import client
 
 def test_index(client):
     response = client.get('/')
